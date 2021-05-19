3b4bce9f87c25a9038f74cbc651a0d3230a5b699
sainathphad <phadsainath1@gmail.com>
2021-05-19 18:53:36 +0530

    ADDED: Functionality to calculate Monthly Employee Wage

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -9,29 +9,35 @@ public class EmpWageBuilder {
          int IS_PART_TIME = 2;
          int WAGE_PER_HOUR = 20;
          int IS_ABSENT = 0;
+         int MONTHLY_WORKING_DAYS = 20;
         //VARIABLES
         int empHours = 0;
         int dailyEmpWage = 0;
+        int empMonthlyWages = 0;
          System.out.println("Welcome to Employee Wage Computation Program");
 
              //COMPUTATION
-         int empCheck = (int) (Math.random() * 10 ) % 3;
-         switch (empCheck)  {
-             case 1:
-                     System.out.println("Employee is Present");
-                     empHours = 8;
-                break;
-             case 2:
-                     System.out.println("Employee Present Part Time");
-                     empHours = 4;
-                break;
-             case 0:
-                     System.out.println("Employee Absent");
-                     empHours = 0;
-        }
+        for ( int day = 1; day <= MONTHLY_WORKING_DAYS; day++) {
+             int empCheck = (int) (Math.random() * 10 ) % 3;
+             switch (empCheck)  {
+                 case 1:
+                         System.out.println("Employee is Present");
+                         empHours = 8;
+                         break;
+                 case 2:
+                         System.out.println("Employee Present Part Time");
+                         empHours = 4;
+                         break;
+                 case 0:
+                         System.out.println("Employee Absent");
+                         empHours = 0;
+            }
 
-        //EMPLOYEE WAGE COMPUTATION
-        dailyEmpWage = empHours * WAGE_PER_HOUR;
-        System.out.println("Daily Employee Wage: "+ dailyEmpWage);
+             //EMPLOYEE WAGE COMPUTATION
+             dailyEmpWage = empHours * WAGE_PER_HOUR;
+             empMonthlyWages += dailyEmpWage;
+             System.out.println("Daily Employee Wage: "+ dailyEmpWage);
+        }
+            System.out.println("Monthly Employee Wages: " + empMonthlyWages);
     }
 }
