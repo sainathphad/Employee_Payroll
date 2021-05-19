4fa71f933490dc98873917ae20e02511fdb3f8af
sainathphad <phadsainath1@gmail.com>
2021-05-19 18:20:19 +0530

    MODIFIED: Solved Using Switch Case

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -8,20 +8,27 @@ public class EmpWageBuilder {
          int IS_FULL_TIME = 1;
          int IS_PART_TIME = 2;
          int WAGE_PER_HOUR = 20;
-
+         int IS_ABSENT = 0;
         //VARIABLES
         int empHours = 0;
         int dailyEmpWage = 0;
          System.out.println("Welcome to Employee Wage Computation Program");
 
              //COMPUTATION
-         double empCheck = Math.floor(Math.random() * 10) % 3;
-         if(empCheck == IS_FULL_TIME)
-            empHours = 8;
-         else if(empCheck == IS_PART_TIME)
-            empHours = 4;
-        else
-            empHours = 0;
+         int empCheck = (int) (Math.random() * 10 ) % 3;
+         switch (empCheck)  {
+             case 1:
+                     System.out.println("Employee is Present");
+                     empHours = 8;
+                break;
+             case 2:
+                     System.out.println("Employee Present Part Time");
+                     empHours = 4;
+                break;
+             case 0:
+                     System.out.println("Employee Absent");
+                     empHours = 0;
+        }
 
         //EMPLOYEE WAGE COMPUTATION
         dailyEmpWage = empHours * WAGE_PER_HOUR;
