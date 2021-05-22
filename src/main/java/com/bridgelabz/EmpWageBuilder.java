e2db2b91f59493237e2efcf1353e2bf4a2797d34
sainathphad <phadsainath1@gmail.com>
2021-05-22 21:14:38 +0530

    MODIFIED: To Compute Employee Wage for Multiple Companies

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -3,13 +3,18 @@ package com.bridgelabz;
 public class EmpWageBuilder {
 
     //CONSTANTS
-    public static final int IS_FULL_TIME = 1;
-    public static final int IS_PART_TIME = 2;
-    public static final int WAGE_PER_HOUR = 20;
-    public static final int MONTHLY_WORKING_DAYS = 20;
-    public static final int TOTAL_WORKING_HOURS = 100;
+    private static final int IS_FULL_TIME = 1;
+    private static final int IS_PART_TIME = 2;
+    int wagePrHr, maxHr, maxDay;
+    String companyName;
 
-    public static int calculateEmpWage() {
+    public EmpWageBuilder(String companyName, int wagePrHr, int maxHr, int maxDay) {
+        this.companyName = companyName;
+        this.wagePrHr = wagePrHr;
+        this.maxHr = maxHr;
+        this.maxDay = maxDay;
+    }
+    private int calculateEmpWage() {
         //VARIABLES
         int empHours;
         int dailyEmpWage;
@@ -18,36 +23,36 @@ public class EmpWageBuilder {
         int workingDays = 0;
 
         //COMPUTATION
-        while (  workingHours < TOTAL_WORKING_HOURS && workingDays < MONTHLY_WORKING_DAYS )    {
+
+        while (  workingHours < maxHr && workingDays < maxDay )    {
             workingDays++;
             int empCheck = (int) (Math.random() * 10 ) % 3;
             switch (empCheck)  {
                 case IS_FULL_TIME:
-                    System.out.println("Employee is Present");
                     empHours = 8;
                     break;
                 case IS_PART_TIME:
-                    System.out.println("Employee Present Part Time");
                     empHours = 4;
                     break;
                 default:
-                    System.out.println("Employee Absent");
                     empHours = 0;
             }
             workingHours += empHours;
             //EMPLOYEE WAGE COMPUTATION
-            dailyEmpWage = empHours * WAGE_PER_HOUR;
+            dailyEmpWage = empHours * wagePrHr;
             empMonthlyWages += dailyEmpWage;
-            System.out.println("Daily Employee Wage: " + dailyEmpWage);
         }
-        System.out.println("Monthly Employee Wages: " + empMonthlyWages);
         return empMonthlyWages;
     }
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation Program");
-        calculateEmpWage();
+        EmpWageBuilder dMart = new EmpWageBuilder("dMart",20,100,20);
+        System.out.println("Employee Wage for dMart: " + dMart.calculateEmpWage());
+        EmpWageBuilder cg = new EmpWageBuilder("cg",10,50,10);
+        System.out.println("Employee Wage for cg: " + cg.calculateEmpWage());
+
 
     }
 }
