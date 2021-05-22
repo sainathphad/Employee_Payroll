9f9e59deaf18b68c791a6b64c93cc656aa0a8dc2
sainathphad <phadsainath1@gmail.com>
2021-05-22 16:42:27 +0530

    REFACTORED: Using Class Method

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -3,15 +3,13 @@ package com.bridgelabz;
 public class EmpWageBuilder {
 
     //CONSTANTS
-    static final int IS_FULL_TIME = 1;
-    static final int IS_PART_TIME = 2;
-    static final int WAGE_PER_HOUR = 20;
-    static final int MONTHLY_WORKING_DAYS = 20;
-    static final int TOTAL_WORKING_HOURS = 100;
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int MONTHLY_WORKING_DAYS = 20;
+    public static final int TOTAL_WORKING_HOURS = 100;
 
-    public static void main(String[] args) {
-
-
+    public static int calculateEmpWage() {
         //VARIABLES
         int empHours;
         int dailyEmpWage;
@@ -19,31 +17,37 @@ public class EmpWageBuilder {
         int workingHours = 0;
         int workingDays = 0;
 
-        System.out.println("Welcome to Employee Wage Computation Program");
-
-             //COMPUTATION
+        //COMPUTATION
         while (  workingHours < TOTAL_WORKING_HOURS && workingDays < MONTHLY_WORKING_DAYS )    {
-                workingDays++;
-             int empCheck = (int) (Math.random() * 10 ) % 3;
-             switch (empCheck)  {
-                 case IS_FULL_TIME:
-                         System.out.println("Employee is Present");
-                         empHours = 8;
-                         break;
-                 case IS_PART_TIME:
-                         System.out.println("Employee Present Part Time");
-                         empHours = 4;
-                         break;
-                 default:
-                         System.out.println("Employee Absent");
-                         empHours = 0;
+            workingDays++;
+            int empCheck = (int) (Math.random() * 10 ) % 3;
+            switch (empCheck)  {
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Present");
+                    empHours = 8;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee Present Part Time");
+                    empHours = 4;
+                    break;
+                default:
+                    System.out.println("Employee Absent");
+                    empHours = 0;
             }
-                    workingHours += empHours;
-             //EMPLOYEE WAGE COMPUTATION
-             dailyEmpWage = empHours * WAGE_PER_HOUR;
-             empMonthlyWages += dailyEmpWage;
-             System.out.println("Daily Employee Wage: "+ dailyEmpWage);
+            workingHours += empHours;
+            //EMPLOYEE WAGE COMPUTATION
+            dailyEmpWage = empHours * WAGE_PER_HOUR;
+            empMonthlyWages += dailyEmpWage;
+            System.out.println("Daily Employee Wage: " + dailyEmpWage);
         }
-            System.out.println("Monthly Employee Wages: " + empMonthlyWages);
+        System.out.println("Monthly Employee Wages: " + empMonthlyWages);
+        return empMonthlyWages;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program");
+        calculateEmpWage();
+
     }
 }
