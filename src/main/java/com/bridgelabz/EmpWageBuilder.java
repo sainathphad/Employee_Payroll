94cf48139e64624c38af4d672ee6a8794ef79451
sainathphad <phadsainath1@gmail.com>
2021-05-24 14:19:20 +0530

    REFACTORED: Emp Wage For Multiple Companies Using Class method

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -5,16 +5,9 @@ public class EmpWageBuilder {
     //CONSTANTS
     private static final int IS_FULL_TIME = 1;
     private static final int IS_PART_TIME = 2;
-    int wagePrHr, maxHr, maxDay;
-    String companyName;
 
-    public EmpWageBuilder(String companyName, int wagePrHr, int maxHr, int maxDay) {
-        this.companyName = companyName;
-        this.wagePrHr = wagePrHr;
-        this.maxHr = maxHr;
-        this.maxDay = maxDay;
-    }
-    private int calculateEmpWage() {
+
+    public static int calculateEmpWage(String companyName, int wagePrHr, int maxHr, int maxDay) {
         //VARIABLES
         int empHours;
         int dailyEmpWage;
@@ -42,16 +35,14 @@ public class EmpWageBuilder {
             dailyEmpWage = empHours * wagePrHr;
             empMonthlyWages += dailyEmpWage;
         }
+        System.out.println("Monthly Emp Wage For company: " +companyName+ " is: " +empMonthlyWages);
         return empMonthlyWages;
     }
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Employee Wage Computation Program");
-        EmpWageBuilder dMart = new EmpWageBuilder("dMart",20,100,20);
-        System.out.println("Employee Wage for dMart: " + dMart.calculateEmpWage());
-        EmpWageBuilder cg = new EmpWageBuilder("cg",10,50,10);
-        System.out.println("Employee Wage for cg: " + cg.calculateEmpWage());
+        calculateEmpWage("Dmart",20,5,10);
+        calculateEmpWage("CG",10,6,20);
 
 
     }
