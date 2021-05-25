a32e3e4716239d4996ea30da05957bec634d6166
sainathphad <phadsainath1@gmail.com>
2021-05-25 21:02:21 +0530

    REFACTORED: To Manage Wage for Multiple Companies Using Array

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -6,31 +6,15 @@ public class EmpWageBuilder {
     private static final int IS_FULL_TIME = 1;
     private static final int IS_PART_TIME = 2;
 
-    int wagePrHr, maxHr, maxDay, empMonthlyWages;
-    String companyName;
-
-    public EmpWageBuilder(String companyName, int wagePrHr, int maxHr, int maxDay) {
-        this.companyName = companyName;
-        this.wagePrHr = wagePrHr;
-        this.maxHr = maxHr;
-        this.maxDay = maxDay;
-    }
-
-    @Override
-    public String toString() {
-        return "Toatal Emp Wage for Company: " +companyName+ " is: " + empMonthlyWages;
-    }
-
-    public void calculateEmpWage() {
+    //EMPLOYEE WAGE COMPUTATION
+    public void calculateEmpWage(CompanyEmpWage companyEmpWage) {
         //VARIABLES
         int empHours;
         int dailyEmpWage;
         int workingHours = 0;
         int workingDays = 0;
 
-        //COMPUTATION
-
-        while (  workingHours < maxHr && workingDays < maxDay )    {
+        while (  workingHours < companyEmpWage.maxHr && workingDays < companyEmpWage.maxDay )    {
             workingDays++;
             int empCheck = (int) (Math.random() * 10 ) % 3;
             switch (empCheck)  {
@@ -44,22 +28,42 @@ public class EmpWageBuilder {
                     empHours = 0;
             }
             workingHours += empHours;
-            //EMPLOYEE WAGE COMPUTATION
-            dailyEmpWage = empHours * wagePrHr;
-            empMonthlyWages += dailyEmpWage;
+
+            dailyEmpWage = empHours * companyEmpWage.wagePrHr;
+
+            companyEmpWage.empMonthlyWages += dailyEmpWage;
         }
+        System.out.println( "Total Wage Of " + companyEmpWage.companyName + " Employee is: " + companyEmpWage.empMonthlyWages );
 
     }
-
+    // MAIN METHOD
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Employee Wage Computation Program");
-        EmpWageBuilder dMart = new EmpWageBuilder("dMart",20,100,20);
-        dMart.calculateEmpWage();
-        System.out.println("Employee Wage for dMart: " + dMart);
-        EmpWageBuilder cg = new EmpWageBuilder("cg",15,60,10);
-        cg.calculateEmpWage();
-        System.out.println("Employee Wage for cg: " + cg);
+        System.out.println( " Welcome to Employee Wage Computation Program " );
+
+        EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+        // DECLARATION OF ARRAY
+        CompanyEmpWage[] company = new CompanyEmpWage[3];
+        // OBJECT CREATION
+        company[0] = new CompanyEmpWage("DMart",20,40,20);
+        empWageBuilder.calculateEmpWage(company[0]);
+        company[1] = new CompanyEmpWage("Bridgelabz",25,60,20);
+        empWageBuilder.calculateEmpWage(company[1]);
+        company[2] = new CompanyEmpWage("Reliance",15,45,25);
+        empWageBuilder.calculateEmpWage(company[2]);
 
     }
+}
+// CLASS COMPANY EMP WAGE
+class CompanyEmpWage {
+    int wagePrHr, maxHr, maxDay, empMonthlyWages;
+    String companyName;
+    //CONSTRUCTOR
+    public CompanyEmpWage(String companyName, int wagePrHr, int maxHr, int maxDay) {
+        this.companyName = companyName;
+        this.wagePrHr = wagePrHr;
+        this.maxHr = maxHr;
+        this.maxDay = maxDay;
+    }
+
 }
